6a0e82683f393d41a0d39a374828e01c325200c8
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-21 10:15:22 +0600

    added BrowserRouter, Provider store, PersistGate;

--- frontend-app/src/main.tsx
+++ frontend-app/src/main.tsx
@@ -1,9 +1,19 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
+import {PersistGate} from 'redux-persist/integration/react';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {persistor, store} from './app/store';
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App/>
-    </StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App/>
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>,
 );
