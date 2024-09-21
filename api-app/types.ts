4231f05f7147a5f6776bd217102a41cbd8184670
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-21 12:32:52 +0600

    refactor UserFields; added ProductFields, CategoryFields;

--- api-app/types.ts
+++ api-app/types.ts
@@ -1,14 +1,30 @@
 import {Model} from 'mongoose';
 
 export interface UserFields {
-    username: string;
-    password: string;
-    token: string;
+  username: string;
+  password: string;
+  name: string;
+  phoneNumber: string;
+  token: string;
 }
 
 export interface UserMethods {
-    checkPassword(password: string): Promise<boolean>;
-    generateToken(): void;
+  checkPassword(password: string): Promise<boolean>;
+
+  generateToken(): void;
 }
 
 export type UserModel = Model<UserFields, {}, UserMethods>
+
+export interface ProductFields {
+  user: string;
+  category: string;
+  title: string;
+  price: number;
+  description: string;
+  image: string | null;
+}
+
+export interface CategoryFields {
+  title: string;
+}
