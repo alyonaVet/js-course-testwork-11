0c84805a512a41e65e7b0c8249a7ece87a545414
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-21 18:53:26 +0600

    added navigate to all products;

--- frontend-app/src/features/products/AddProduct.tsx
+++ frontend-app/src/features/products/AddProduct.tsx
@@ -4,14 +4,17 @@ import {useAppDispatch, useAppSelector} from '../../app/hooks';
 import {addProduct, fetchProducts} from './productsThunk';
 import {ProductFields} from '../../types';
 import {selectProductsCreating} from './productsSlice';
+import {useNavigate} from 'react-router-dom';
 
 const AddProduct = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const productsCreating = useAppSelector(selectProductsCreating);
 
   const submitHandler = async (productData: ProductFields) => {
    await dispatch(addProduct(productData));
    await dispatch(fetchProducts());
+   navigate('/');
   };
 
   return (
