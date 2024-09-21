5ebc928928ae265fb9fd7ed775aceb39444281d1
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-21 17:19:58 +0600

    created route new-product, /products/:id;

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -5,8 +5,19 @@ import Register from './features/users/components/Register';
 import Login from './features/users/components/Login';
 import NotFoundPage from './UI/NotFoundPage/NotFoundPage';
 import Products from './features/products/Products';
+import AddProduct from './features/products/AddProduct';
+import {useEffect} from 'react';
+import {useAppDispatch} from './app/hooks';
+import {fetchCategories} from './features/categories/categoriesThunk';
+import OneProduct from './features/products/OneProduct';
 
 const App = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch]);
+
   return (
     <>
       <header>
@@ -17,6 +28,8 @@ const App = () => {
           <Route path="/" element={<Products />}/>
           <Route path="/register" element={<Register/>}/>
           <Route path="/login" element={<Login/>}/>
+          <Route path="/new-product" element={<AddProduct/>}/>
+          <Route path="/products/:id" element={<OneProduct/>}/>
           <Route path="*" element={<NotFoundPage/>}/>
         </Routes>
       </main>
