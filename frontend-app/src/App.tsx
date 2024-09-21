c5c32cf20a17647be19a4588f1229392922910de
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-21 10:19:28 +0600

    added Routes;

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css';
+import AppToolbar from './UI/AppToolbar/AppToolbar';
+import {Route, Routes} from 'react-router-dom';
+import Register from './features/users/components/Register';
+import Login from './features/users/components/Login';
+import NotFoundPage from './UI/NotFoundPage/NotFoundPage';
 
 const App = () => {
-    return (
-        <>
-            Hello, world
-        </>
-    )
+  return (
+    <>
+      <header>
+        <AppToolbar/>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
+      </main>
+    </>
+  );
 };
 
 export default App;
