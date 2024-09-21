14779a480194c2d2708daf46ec983adac50a94a4
alyonaVet <a.v.alenka2015@gmail.com>
2024-09-21 17:47:26 +0600

    added dedicated category page;

--- frontend-app/src/features/products/Products.tsx
+++ frontend-app/src/features/products/Products.tsx
@@ -1,18 +1,28 @@
-import {Box, Container, Stack} from '@mui/material';
+import {Box, Container, Stack, Typography} from '@mui/material';
 import CategoriesMenu from '../categories/components/CategoryMenu';
 import {useAppDispatch, useAppSelector} from '../../app/hooks';
 import {selectCategories} from '../categories/categoriesSlice';
 import {useEffect} from 'react';
 import Product from './components/Product';
 import {selectProducts} from './productsSlice';
-import {useNavigate} from 'react-router-dom';
+import {useNavigate, useParams} from 'react-router-dom';
 import {fetchProducts} from './productsThunk';
+import {CategoryCredentials, ProductCredentials} from '../../types';
 
 const Products = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const categories = useAppSelector(selectCategories);
   const products = useAppSelector(selectProducts);
+  const {catId} = useParams();
+
+  const catFilter = catId
+    ? (product: ProductCredentials) => product.category._id === catId
+    : () => true;
+
+  const catTitle = catId
+    ? categories.find((cat: CategoryCredentials) => cat._id === catId)?.title
+    : 'All Categories';
 
   useEffect(() => {
     dispatch(fetchProducts());
@@ -23,14 +33,16 @@ const Products = () => {
   };
 
   return (
-    <Container maxWidth="xl">
+    <Container maxWidth="xl" sx={{mt: 3}}>
       <Stack direction="row">
         <Box sx={{flexGrow: 0, flexShrink: 0, width: '25%'}}>
           <CategoriesMenu categories={categories}/>
         </Box>
         <Box sx={{flexGrow: 0, flexShrink: 0, width: '75%'}}>
+          <Typography component="h1" variant="h5" sx={{pl:1}}>{catTitle}</Typography>
+
           <Stack direction="row" flexWrap={'wrap'}>
-            {products.map((product) => (
+            {products.filter(catFilter).map((product) => (
               <Product
                 key={product._id}
                 product={product}
